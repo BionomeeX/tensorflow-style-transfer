6d5e17b83f32aa86bdf12a165e2a25f44a3b621e
Clement Carre <clement.carre@bionomeex.com>
2021-08-07 18:24:19 +0200

    Update styletransfer

--- styletransfer.py
+++ styletransfer.py
@@ -5,6 +5,21 @@ import os
 
 hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
 
 parser = argparse.ArgumentParser(
     usage="%(Style Transfer)s [OPTION] -I image 0 -J image 1",
@@ -18,17 +33,19 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-img0 = tf.io.read_file(args.I)
-img0 = tf.image.decode_image(img0, channels=3)
-img0 = tf.image.convert_image_dtype(img0, tf.float32)
-img1 = tf.io.read_file(args.J)
-img1 = tf.image.decode_image(img1, channels=3)
-img1 = tf.image.convert_image_dtype(img1, tf.float32)
+img0 = load_img(args.I)
+img1 = load_img(args.J)
+
+
+print(img0.shape)
+print(img1.shape)
+
 
 image0 = hub_model(tf.constant(img0), tf.constant(img1))[0]
 image1 = hub_model(tf.constant(img1), tf.constant(img0))[0]
 
-outfile = ".".join(os.path.basename(args.I).split(".")[:-1])
+outfileI = ".".join(os.path.basename(args.I).split(".")[:-1]) + ".jpg"
+outfileJ = ".".join(os.path.basename(args.J).split(".")[:-1]) + ".jpg"
 
-tf.io.write_file(outfile + "_0.jpg", tf.image.encode_jpeg(tf.image.convert_image_dtype(image0[0,], tf.uint8)))
-tf.io.write_file(outfile + "_1.jpg", tf.image.encode_jpeg(tf.image.convert_image_dtype(image1[0,], tf.uint8)))
+tf.io.write_file(outfileI, tf.image.encode_jpeg(tf.image.convert_image_dtype(image0[0,], tf.uint8)))
+tf.io.write_file(outfileJ, tf.image.encode_jpeg(tf.image.convert_image_dtype(image1[0,], tf.uint8)))
